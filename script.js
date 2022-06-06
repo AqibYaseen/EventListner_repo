1c0c0eaae2f591579acc1b971c7fcf5b43013d2d
AQIBYASEENBHAT <AQIBYASEENBHAT@GMAIL.COM>
2022-06-06 13:06:37 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+
+
+let div1 = document.getElementById("mainDiv");
+
+let btn = document.getElementById("btn");
+
+btn.onmouseover = () =>{
+
+}
+
+btn.onmousedown = () => {
+    div1.innerHTML = "clicked"
+
+}
+
+btn.ondblclick = () => {
+    div1.innerHTML = "double_cicked"
+
+}
+
+btn.onmouseout = () => {
+    div1.innerHTML = "mouse_out_of_buton"
+
+}
+
+btn.onmouseover = () => {
+    div1.innerHTML = "mouse_on_button"
+
+}
+
+
